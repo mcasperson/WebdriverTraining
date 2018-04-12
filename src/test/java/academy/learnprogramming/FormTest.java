23f364fdd51cbe055add942bf627e784eed59f27
Matthew Casperson <matthewcasperson@gmail.com>
2018-04-12 17:04:54 +1000

    Finished up the tests by id

--- src/test/java/academy/learnprogramming/FormTest.java
+++ src/test/java/academy/learnprogramming/FormTest.java
@@ -15,9 +15,30 @@ public class FormTest {
         try {
             automatedBrowser.init();
             automatedBrowser.goTo(FormTest.class.getResource("/form.html").toURI().toString());
+
             automatedBrowser.clickElementWithId("button_element");
-            String message = automatedBrowser.getTextFromElementWithId("message");
-            assertEquals("Button Clicked", message);
+            assertEquals("Button Clicked", automatedBrowser.getTextFromElementWithId("message"));
+
+            automatedBrowser.populateElementWithId("text_element", "test text");
+            assertEquals("Text Input Changed", automatedBrowser.getTextFromElementWithId("message"));
+
+            automatedBrowser.populateElementWithId("textarea_element", "test text");
+            assertEquals("Text Area Changed", automatedBrowser.getTextFromElementWithId("message"));
+
+            automatedBrowser.selectOptionByTextFromSelectWithId("Option 2.1", "select_element");
+            assertEquals("Select Changed", automatedBrowser.getTextFromElementWithId("message"));
+
+            automatedBrowser.clickElementWithId("radio3_element");
+            assertEquals("Radio Button Changed", automatedBrowser.getTextFromElementWithId("message"));
+
+            automatedBrowser.clickElementWithId("checkbox2_element");
+            assertEquals("Checkbox Changed", automatedBrowser.getTextFromElementWithId("message"));
+
+            automatedBrowser.clickElementWithId("image_element");
+            assertEquals("Image Clicked", automatedBrowser.getTextFromElementWithId("message"));
+
+            automatedBrowser.clickElementWithId("div_element");
+            assertEquals("Div Clicked", automatedBrowser.getTextFromElementWithId("message"));
         } finally {
             automatedBrowser.destroy();
         }
